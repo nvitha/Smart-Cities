bce4137ab0d5633d35c997339f77694e5d95b4f2
Malcolm Chaney <chaneym@gmail.com>
2016-11-15 00:18:06 -0800

    Inital commit of framework

--- django/volttron/views/logging_view.py
+++ django/volttron/views/logging_view.py
@@ -2,8 +2,8 @@ import logging
 
 from django.http import HttpResponse
 from django.template import loader
-from volttron.models import Topics, ButtonPresses
-from volttron.tables import RivaTable, TopicsTable
+from volttron.models import ButtonPresses
+from volttron.tables import RivaTable
 from django_tables2 import RequestConfig
 
 # python debug logger
@@ -13,13 +13,13 @@ logger = logging.getLogger(__name__)
 def logging(request):
     # define variable dict to pass into the template
 
-    table = TopicsTable(Topics.objects.all())
+    table = RivaTable(ButtonPresses.objects.all(), order_by='-pressed_datetime')
     RequestConfig(request).configure(table)
 
+    context = {'table': table}
     # getting our template
     template = loader.get_template('logging.html')
 
-    context = {'table': table}
     # rendering the template in HttpResponse
     return HttpResponse(template.render(context, request))
 
