a9d2825d1aed939b456f554692fff677bdb4d53b
Malcolm Chaney <chaneym@gmail.com>
2016-11-27 15:54:10 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	django/templates/metrics.html

--- django/smartcity/urls.py
+++ django/smartcity/urls.py
@@ -26,7 +26,22 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # Home Page
     url(r'^$', index_view.index, name='index'),
-    url(r'^starttest/$', index_view.publish, name='volttron_publish'),
+
+    # Index Smart Meter
+    url(r'^refresh_status$', index_view.index, name='refresh_status'),
+    url(r'^start_thrash/$', index_view.start_thrash_test, name='start_thrash'),
+    url(r'^stop_thrash/$', index_view.stop_thrash_test, name='stop_thrash'),
+    url(r'^start_random/$', index_view.start_random_test, name='start_random'),
+    url(r'^stop_random/$', index_view.stop_random_test, name='stop_random'),
+    url(r'^start_uniform/$', index_view.start_uniform_test, name='start_uniform'),
+    url(r'^stop_uniform/$', index_view.stop_uniform_test, name='stop_uniform'),
+
+    # Index BAC
+    url(r'^mode_one/$', index_view.mode_one, name='mode_one'),
+    url(r'^mode_two/$', index_view.mode_one, name='mode_two'),
+    url(r'^mode_three/$', index_view.mode_one, name='mode_three'),
+    url(r'^mode_four/$', index_view.mode_one, name='mode_four'),
+    
     # Metrics View
     url(r'^metrics/', metrics_view.metrics, name='metrics'),
     # Logging View
